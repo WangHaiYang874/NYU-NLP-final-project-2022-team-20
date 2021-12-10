561e12372fb87c94329052d90cbebf0a56299d7f
Haiyang Wang <hw1927@nyu.edu>
2021-12-10 15:14:51 +0800

    features completed

--- src/Features.py
+++ src/Features.py
@@ -6,43 +6,40 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import pickle
 import datetime
 import gensim
-import emoji
-
+import numpy as np
+from gensim.utils import simple_preprocess
 
 class Features:
     # class attr
     
     wnl = WordNetLemmatizer()
-    reFilter = re.compile(
-        "(http.*?([ ]|\|\|\||$))|" + r"((:|;).)|" + 
-        '([' + re.escape(string.punctuation) + '])|' + 
-        '((\[|\()*\d+(\]|\))*)|' + '''([’‘“\.”'"`…–])|''' +
-        '([^(\w|\s)])|' + '(gt|lt)|'
-    )
+    # reFilter = re.compile(
+    #     "(http.*?([ ]|\|\|\||$))|" + r"((:|;).)|" + 
+    #     '([' + re.escape(string.punctuation) + '])|' + 
+    #     '((\[|\()*\d+(\]|\))*)|' + '''([’‘“\.”'"`…–])|''' +
+    #     '([^(\w|\s)])|' + '(gt|lt)|'
+    # )
+    
     
     def __init__(self, series, stopwords):
         
-        # inputs
+        # datas
         self.raw_series = series
         self.stop_words = stopwords
-        
-        # cleaned
+        with open('Emoticon_Dict.p', 'rb') as fp:
+            self.emoticons = pickle.load(fp).keys()
         self.cleaned_series = None
         
-        # tfidf
+        # models
+        
         self.vectorizer = CountVectorizer(min_df=0.0005)
         self.tfidf = TfidfTransformer()
-        
-        # topics 
         self.lda_model = None
+        self.dictionary = None
         
-        # emoticons
-        with open('Emoticon_Dict.p', 'rb') as fp:
-            self.emoticon_dict = pickle.load(fp)
-        self.emoticon_pattern = re.compile('|'.join(k for k in self.emoticon_dict))
-        # todo
     
-    # CLEANING
+    
+    # BUILDING
     def clean_sentence(self,sentence):
         
         # lower and only ascii
@@ -72,89 +69,100 @@ class Features:
         sentence = re.sub('[%s]' % re.escape(string.punctuation), ' ', sentence)
         sentence = re.sub(r'\w*\d+\w*', '', sentence)
         sentence = re.sub('\s{2,}', " ", sentence)
-        
-        
+                
         return sentence
-        
     
-    def clean(self):
-        self.cleaned_series = self.raw_series.apply(self.clean_sentence)
-        
-        
-    # TFIDF 
     def build_tfidf(self):
-        assert(feat.cleaned_series.any())
+        assert(self.cleaned_series.any())
                 
         X = self.vectorizer.fit_transform(
             self.cleaned_series)
         
         self.tfidf.fit(X)
-    
-    def get_tfidf(self, series):
+        
+    def build_topics(self):
+        
+        processed_doc = [simple_preprocess(doc) for doc in self.cleaned_series]
+        self.dictionary = gensim.corpora.Dictionary(processed_doc)
+        self.dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n= 100000)
+        bow_corpus = [self.dictionary.doc2bow(doc) for doc in processed_doc]
+        self.lda_model = gensim.models.LdaMulticore(
+            bow_corpus, 
+            num_topics = 200, 
+            id2word = self.dictionary,                                    
+            passes = 10,
+            workers = 2)
+            
+    def save_self(self):
+        # this will not save the raw_series and cleaned series
+        
+        suffix = str(datetime.date.today())
+        prefix = '../models/features'
+        
+        # setting the data to none, avoiding to save a large model file with all the datas. 
+        
+        # this is the debug version of the model, where all datas are saved
+        with open(prefix + suffix + '.debug', 'wb') as f:
+            pickle.dump(self ,f)
+        
+        
+        # setting all the datas to non to save only the models. 
+        self.raw_series = None
+        self.cleaned_series = None
+        self.stop_words = None
+        self.emoticons = None
+        
+        with open(prefix + suffix + '.model', 'wb') as f:
+            pickle.dump(self ,f)
+            
+        return prefix + suffix + '.debug', 'wb'
+        
+    def build_model(self):
+        print('cleaning the copora')
+        self.cleaned_series = self.raw_series.apply(self.clean_sentence)
+        print('building tfidf model')
+        self.build_tfidf()
+        print('building lda topic model')
+        self.build_topics()
+        print('model built, saving it')
+        path = self.save_self()
+        print('model saved at:')
+        return path
+
+    # GETTING FEATURES
+    def get_tfidf(self, cleaned_series):
         return self.tfidf.transform(
-            self.vectorizer(series.apply(self.clean_sentence)))
-    
-    # EMOTICONS
+            self.vectorizer(cleaned_series))
+
     def extract_emoticons(self,s):
         '''
         extract all the emoticons from a string s
         should return an counter or an array? 
         '''
-        emoticons = "(\:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)"
-        another_one = "¯\\\_(ツ)_/¯"
-        emojis = re.findall(emoticons, s)
-        emojis.extend(re.findall(another_one, s))
-        #returns a list of all emojis found in string s
-        return emojis
+        ret = [len(re.findall(emoticon, s)) for emoticon in self.emoticons]
+        ret.append(sum(ret))
+        return ret
     
-    def build_emoticons(self):
-        self.emoticon_series = self.raw_series.apply(self.extract_emoticons)
-        # todo: transform this into a numpy array? that could be read into other dataset. 
-    
-    def get_emoticons(self, series):
-        return self.raw_series.apply(self.extract_emoticons)
-        # todo: transform this into a numpy array? that could be read into other dataset. 
-
-
-    # TOPICS
-
-    def build_topics(self):
-        processed_doc = self.cleaned_series.apply(lambda row: nltk.word_tokenize(row['sentences']), axis=1)
-        dictionary = gensim.corpora.Dictionary(processed_doc)
-        dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n= 100000)
-        bow_corpus = [dictionary.doc2bow(doc) for doc in processed_doc]
-        self.lda_model = gensim.models.LdaMulticore(bow_corpus, 
-                                   num_topics = 200, 
-                                   id2word = dictionary,                                    
-                                   passes = 10,
-                                   workers = 2)
+    def get_emoticons(self, raw_series):
+        return np.array(list(map(self.extract_emoticons, raw_series.values)))
         
     def get_topics(self,series):
-        return self.lda_model
         
-    
-    
-    
-
-    # Aggregate
-    # TODO: update after the features are completed.         
+        processed_doc = [simple_preprocess(doc) for doc in series]
+        bows = [self.dictionary.doc2bow(doc) for doc in processed_doc]
+        ret = []
+        for doc in bows:
+            prob = self.lda_model.get_document_topics(doc, minimal_probability=0)
+            prob.sort(key=lambda x:x[0])
+            prob = [i[1] for i in prob]
+            ret.append(prob)
+        
+        return np.array(ret)
+        
     def get_features(self,series):
-        # TODO, once topics and emoticons are finished, add them here. 
-        return self.get_tfidf(self,series)
-    
-    def save_self(self):
-        # this will not save the raw_series and cleaned series
-        
-        suffix = str(datetime.date.today())
-        prefix = '../models/'
-        
-        d = (self.raw_series, self.cleaned_series)
-        self.raw_series = None
-        self.cleaned_series = None
-        self.emoticon_series = None
-
-        with open('../models/features' +str(datetime.date.today()) + '.model', 'wb') as f:
-            pickle.dump(self ,f)
-        
-        with open(prefix +  "data_series" + suffix + ".model",'wb') as f:
-            pickle.dump(d,f)
+        cleaned_series = series.apply(self.clean_sentence)
+        tfidfs = self.get_tfidf(cleaned_series)
+        emoticons = self.get_emoticons(series)
+        topics = self.get_topics(cleaned_series)
+        return zip(tfidfs,emoticons,topics)
+        # TODO: I need to reshaping this
